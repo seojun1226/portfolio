096e392b0682e4d13c69f189b63aa06025ccba4d
Seo Jun <94329410+seojun1226@users.noreply.github.com>
2021-12-29 13:58:24 +0900

    Delete js directory

--- js/discordTooltip.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('DOMContentLoaded', function() {
-    document.querySelector(".discordIcon").addEventListener('mouseover', () => {
-        document.querySelector(".discordTooltip").style.width = "70px";
-    });
-    
-    document.querySelector(".discordIcon").addEventListener('mouseout', () => {
-        document.querySelector(".discordTooltip").style.width = "0px";
-    });
-});
--- js/skill.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('DOMContentLoaded', function() {
-    const progress = document.querySelectorAll(".progressBg");
-
-    setTimeout(()=>{
-        progress[0].style.width = "70%";
-        progress[1].style.width = "50%";
-        progress[2].style.width = "20%";
-    }, 1);
-});
